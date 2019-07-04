aaef9d5c59012ae82c6ce2719638119eb33d6ea2
Deepakumar S <51949620+Deepakumar1806@users.noreply.github.com>
2019-07-04 11:14:53 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,11 +1,11 @@
-num = int(input())
-sum = 0
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-if num == sum:
+num1 = int(input())
+sum1 = 0
+temp1 = num1
+while temp1 > 0:
+   digit = temp1 % 10
+   sum1 += digit ** 3
+   temp1 //= 10
+if num1 == sum1:
    print("yes")
 else:
    print("no")
